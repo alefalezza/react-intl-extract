8cf0a8ec3ca4099284857339d4dea3ded26807b3
Ryan J. Peterson <rpeterson@serenitysoft.com>
2019-04-23 08:43:07 -0400

    Adding -e --extension as CLI option

--- cli.js
+++ cli.js
@@ -12,6 +12,7 @@ program
   .allowUnknownOption()
   .option('-l,--locales <items>', 'list of output languages, default en,it', list)
   .option('-s,--src [value]', 'source directory, default /src')
+  .option('-e,--extensions <items>', 'extensions of files to be searched', list)
   .option('-o,--output [value]', 'output directory: where dictionaries are saved, default /locales')
   .parse(process.argv);
 
--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@ const cwd = process.cwd();
 
 module.exports = {
     locales: ['en', 'it'],
+    extensions: ['jsx', 'tsx'],
     src: path.join(cwd, 'src'),
     output: path.join(cwd, 'locales')
 };
